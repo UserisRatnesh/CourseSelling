11e2edc1b8f512de3c6477382b552431f2e26efb
Ratnesh singh <ratneshsingh@Ratneshs-MacBook-Air.local>
2024-01-14 15:07:59 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,140 @@
+const express = require("express");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// for admin to signup
+app.post("/admin/signup", (req,res)=>{
+    var username = req.body.username;
+    var password  = req.body.password;
+    const newAdmin = {
+        id : Math.floor(Math.random()*1000000),
+        username,
+        password
+    }
+
+    fs.readFile("../admins.json", "UTF-8", (_err, data)=>{
+        if(data)
+        {
+            const admins = JSON.parse(data);
+            const updatedAdmins = [];
+            admins.map(admin =>{
+                if(username != admin.username || password != admin.password)
+                {
+                    // admin not present
+                    updatedAdmins.push(admin);
+                    
+
+                }
+            });
+            
+            // if count of old admins and new admins are not same then the admin is present already
+
+            updatedAdmins.push(newAdmin);
+            fs.writeFile("../admins.json", JSON.stringify(updatedAdmins), (err)=>{
+                if(err)
+                {
+                    console.log("err in updating admin list");
+                }
+            });
+        }
+    });
+    res.status(200).json(newAdmin);
+})
+
+// for admin to signin
+app.post("/admin/login", (req, res)=>{
+    
+})
+
+// for posting courses by admin
+var count = 1;
+app.post("/admin/course", (req, res)=>{
+    let title = req.body.title;
+    let description = req.body.description;
+    let price = req.body.price;
+    const newCourse = {
+        id : count,
+        title,
+        description,
+        price
+    };
+    count = count +1;
+
+    // first read the file then add this new course to existing course
+    fs.readFile("../courses.json", "UTF-8", (_err, data) =>{
+        if(data)
+        {
+            // convertes the string data into json
+            const courses = JSON.parse(data);
+            courses.push(newCourse);
+
+            // adding the updated course list to the file
+            fs.writeFile("../courses.json", JSON.stringify(courses), (err)=>{
+                if(err)
+                {
+                    console.log("err in rewriting");
+                }
+            });
+        }
+        else{
+            console.log("could not read file");
+        }
+    })
+    
+    res.status(200).send(newCourse);
+});
+
+// to get all courses added by the admin
+app.get("/admin/courses", (_req, res)=>{
+
+    
+    fs.readFile("../courses.json", "UTF-8", (_err, data)=>{
+        if(data)
+        {
+            const courses = JSON.parse(data);
+            res.status(200).json(courses);
+        }
+        else{
+            res.status(404).send("Could not read file")
+        }
+    });
+    return;
+});
+
+// for deleting course by admin by giving course id
+app.delete("/admin/course/:id", (req, res)=>{
+    var id = parseInt(req.params.id);
+    fs.readFile("../courses.json", "UTF-8", (_err, data)=>{
+        if(data)
+        {
+            const courses = JSON.parse(data);
+            const updatedCouse = [];
+            courses.map(course =>{
+                if(course.id != id)
+                {
+                    updatedCouse.push(course);
+                }
+            });
+
+            // write the updated course to the file
+            fs.writeFile("../courses.json", JSON.stringify(updatedCouse), (err)=>{
+                if(err)
+                {
+                    console.log("err in writing the uodated courses");
+                }
+            })
+        }
+        else{
+            console.log("err in reading file");
+        }
+    })
+    res.status(200).send("Course deleted successfully");
+})
+
+app.listen(3000 , ()=>{
+    console.log("Server running on port 3000");
+})
